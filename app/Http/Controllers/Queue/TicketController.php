670d08039dfb8cae77b95bb82c5085969f4178f6
Pontep Hiranrat <pth25441@gmail.com>
2022-05-25 03:41:25 +0700

    Bug fixed

--- app/Http/Controllers/Queue/TicketController.php
+++ app/Http/Controllers/Queue/TicketController.php
@@ -27,7 +27,8 @@ class TicketController extends Controller
         $this->ticketStatus = $ticketStatus;
     }
 
-    function getTicketGroupByCode(Request $request){
+    function getTicketGroupByCode(Request $request)
+    {
         $ticketGroupCode = $request->input('ticket_group_code', null);
 
         if (!$ticketGroupCode) {
@@ -36,7 +37,9 @@ class TicketController extends Controller
 
         // $lineService = $request->lineService;
 
-        $ticketGroup = TicketGroup::whereTicketGroupCode($ticketGroupCode)->first();
+        $ticketGroup = TicketGroup::select("id", "ticket_group_code", "active", "active_count", "ticket_group_prefix", "description", "queue_setting_id")->whereTicketGroupCode($ticketGroupCode)->with(["queue_setting" => function ($query) {
+            $query->select("id", "display_name", "detail");
+        }])->first();
 
         if (!$ticketGroup) {
             return $this->sendBadResponse(['error' => "CODE_REJECT"], 'Ticket Group Not Found');
@@ -47,10 +50,10 @@ class TicketController extends Controller
         }
 
         $waitingCount = Ticket::whereTicketGroupId($ticketGroup->id)->whereTicketGroupActiveCount($ticketGroup->active_count)->whereStatus($this->ticketStatus['PENDING'])->count();
-        $ticketGroup->waitingCount = $waitingCount;
+        $ticketGroup->waiting_count = $waitingCount;
 
         $lastNumber = Ticket::whereTicketGroupId($ticketGroup->id)->whereTicketGroupActiveCount($ticketGroup->active_count)->max("count");
-        $ticketGroup->nextNumber = $lastNumber + 1;
+        $ticketGroup->next_number = $lastNumber + 1;
 
         return $this->sendOkResponse($ticketGroup);
     }
@@ -80,18 +83,18 @@ class TicketController extends Controller
 
         $profile = $lineService->getProfile();
 
-        $lineMember = LineMember::whereUserId($profile['userId'])->whereHas('line_config', function (Builder $query) use ($lineConfig) {
+        $lineMember = LineMember::whereUserId($profile->userId)->whereHas('line_config', function (Builder $query) use ($lineConfig) {
             $query->whereId($lineConfig->id);
         })->first();
 
         $status = [$this->ticketStatus['PENDING'], $this->ticketStatus['PENDING']];
-        $existsTicket = Ticket::whereTicketGroupId($ticketGroup->id)->whereTicketGroupActiveCount($ticketGroup->ticket_group_active_count)->whereLineMemberId($lineMember->id)->whereIn('status', $status)->count();
+        $existsTicket = Ticket::whereTicketGroupId($ticketGroup->id)->whereTicketGroupActiveCount($ticketGroup->active_count)->whereLineMemberId($lineMember->id)->whereIn('status', $status)->count();
 
-        if($existsTicket != 0) {
+        if ($existsTicket != 0) {
             return $this->sendBadResponse(['error' => "TICKET_EXISTS"], 'Ticket already exists');
         }
 
-        $lastTicket = Ticket::whereTicketGroupId($ticketGroup->id)->whereTicketGroupActiveCount($ticketGroup->ticket_group_active_count)->max('count');
+        $lastTicket = Ticket::whereTicketGroupId($ticketGroup->id)->whereTicketGroupActiveCount($ticketGroup->active_count)->max('count');
 
         $ticket = new Ticket();
 
@@ -99,7 +102,7 @@ class TicketController extends Controller
             $ticket->ticket_group_id = $ticketGroup->id;
             $ticket->line_member_id = $lineMember->id;
             $ticket->status = 1;
-            $ticket->ticket_group_active_count = $ticketGroup->ticket_group_active_count;
+            $ticket->ticket_group_active_count = $ticketGroup->active_count;
             $ticket->count = $lastTicket ? $lastTicket + 1 : 1;
             $ticket->ticket_number = $ticketGroup->ticket_group_prefix . $ticket->count;
             $ticket->save();
@@ -107,22 +110,27 @@ class TicketController extends Controller
             return $this->sendBadResponse(['error' => "TICKET_UNSAVE", 'debug' => $th->getMessage()], 'Cannot create ticket');
         }
 
-        // Send Ticket
-        $this->sendTicket($ticket);
+        $result = Ticket::select("ticket_group_id", "ticket_number")->whereId($ticket->id)->with(["ticket_group" => function ($query) {
+            $query->select("id", "description");
+        }])->first();
 
-        return $this->sendOkResponse($ticket, 'Ticket Added');
+        // Send Ticket
+        $this->sendTicket($ticket->id);
+
+        return $this->sendOkResponse($result, 'Ticket Added');
     }
 
-    private function sendTicket(Ticket $ticket)
+    private function sendTicket(int $ticketId)
     {
+        $ticket = Ticket::whereId($ticketId)->with(["ticket_group.queue_setting", "line_member"])->first();
         $pending_time_object = Carbon::parse($ticket->pending_time);
         $waiting_count = Ticket::whereTicketGroupId($ticket->ticket_group_id)->whereTicketGroupActiveCount($ticket->ticket_group_active_count)->count();
 
-        $description = $ticket->ticket_group()->description;
+        $description = $ticket->ticket_group->description;
         $queue_number = $ticket->ticket_number;
         $pending_time = 'Date: ' . $pending_time_object->format("d F Y") . ' Time: ' . $pending_time_object->format("H:i");
         $waiting_queue = 'Waiting Queue: ' . $waiting_count;
-        $display_name = $ticket->ticket_group()->queue_setting()->display_name;
+        $display_name = $ticket->ticket_group->queue_setting->display_name;
 
         $ticketTemplateStr = $this->ticketTemplate;
 
@@ -133,81 +141,104 @@ class TicketController extends Controller
         );
 
         try {
-            $ticketTemplate = json_decode($ticketTemplateStr);
+            $ticketTemplate = json_decode($ticketTemplateStr, true);
 
-            $lineService = new LineService(['lineUserId' => $ticket->line_member()->user_id]);
+            $lineService = new LineService(['lineUserId' => $ticket->line_member->user_id]);
 
-            $lineService->sendPushMessage($ticketTemplate);
+            $lineService->sendPushMessage([$ticketTemplate]);
         } catch (\Throwable $th) {
             //throw $th;
             Log::error('sendTicket: ' . $th->getMessage());
         }
-
     }
 
     private $ticketTemplate = '
     {
-        "type": "bubble",
-        "header": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "text",
-              "text": "{description}",
-              "size": "12px",
-              "align": "center",
-              "wrap": false,
-              "maxLines": 1,
-              "adjustMode": "shrink-to-fit"
+        "type": "flex",
+        "altText": "Queue Ticket",
+        "contents": {
+            "type": "bubble",
+            "header": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "{display_name}",
+                        "weight": "bold",
+                        "size": "18px",
+                        "align": "center",
+                        "wrap": true,
+                        "adjustMode": "shrink-to-fit",
+                        "maxLines": 2
+                    },
+                    {
+                        "type": "text",
+                        "text": "{description}",
+                        "size": "14px",
+                        "align": "center",
+                        "wrap": false,
+                        "maxLines": 1,
+                        "adjustMode": "shrink-to-fit"
+                    }
+                ]
             },
-            {
-              "type": "text",
-              "text": "{ticket_number}",
-              "size": "24px",
-              "weight": "bold",
-              "align": "center",
-              "wrap": true,
-              "adjustMode": "shrink-to-fit",
-              "maxLines": 1,
-              "margin": "6px"
+            "hero": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "{ticket_number}",
+                        "size": "64px",
+                        "weight": "bold",
+                        "align": "center",
+                        "wrap": true,
+                        "adjustMode": "shrink-to-fit",
+                        "maxLines": 1,
+                        "margin": "6px"
+                    }
+                ]
             },
-            {
-              "type": "text",
-              "text": "{pending_time}",
-              "size": "14px",
-              "align": "center",
-              "margin": "10px"
+            "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "{pending_time}",
+                        "size": "14px",
+                        "align": "center"
+                    }
+                ]
+            },
+            "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    {
+                        "type": "text",
+                        "text": "{waiting_queue}",
+                        "size": "14px",
+                        "align": "center"
+                    }
+                ]
+            },
+            "size": "kilo",
+            "styles": {
+                "header": {
+                    "backgroundColor": "#f4f6f9"
+                },
+                "hero": {
+                    "backgroundColor": "#f0f2f5"
+                },
+                "body": {
+                    "backgroundColor": "#f4f6f9"
+                },
+                "footer": {
+                    "backgroundColor": "#f4f6f9"
+                }
             }
-          ]
-        },
-        "hero": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "text",
-              "text": "{waiting_queue}",
-              "size": "14px",
-              "align": "center"
-            }
-          ]
-        },
-        "body": {
-          "type": "box",
-          "layout": "vertical",
-          "contents": [
-            {
-              "type": "text",
-              "text": "{display_name}",
-              "weight": "bold",
-              "size": "18px",
-              "align": "center",
-              "wrap": true,
-              "adjustMode": "shrink-to-fit",
-              "maxLines": 2
-            }
-          ]
         }
     }';
 
@@ -217,7 +248,7 @@ class TicketController extends Controller
 
         $profile = $lineService->getProfile();
 
-        $lineMember = LineMember::whereUserId($profile['userId'])->first();
+        $lineMember = LineMember::whereUserId($profile->userId)->first();
 
         $status = [$this->ticketStatus['PENDING'], $this->ticketStatus['CALLING']];
         $ticket = Ticket::whereLineMemberId($lineMember->id)->whereIn('status', $status)->orderBy('pending_time', 'desc')->first();
