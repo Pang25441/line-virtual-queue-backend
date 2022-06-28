a325d62eb81f14f37546183a3a5a0c5eabf8b6c0
Pontep Hiranrat <pth25441@gmail.com>
2022-06-28 13:32:45 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,7 @@ class DatabaseSeeder extends Seeder
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => \Illuminate\Support\Str::random(10),
         ]);
-        \App\Models\User::factory(3)->create();
+        // \App\Models\User::factory(3)->create();
 
         $this->call([
             MaBookingStatusSeeder::class,
